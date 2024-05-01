72f0a763c225ce67cf08088366f53c870299de53
Brian Tseng <brianboy9999@gmail.com>
2024-05-01 12:32:09 +0800

    update-Q35

--- 35. Search_Insert_Position.py
+++ 35. Search_Insert_Position.py
@@ -1,12 +1,17 @@
-def searchInsert(nums,target):
-    if target in nums:
-        return nums.index(target)
-    else:
-        ans=0
-        while nums[ans]<target:
-            ans+=1
-            if ans==len(nums)-1:
-                return ans+1
-                break
-        return ans
-print(searchInsert([1,3,5,6],7))
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            ans=0
+            while nums[ans]<target:
+                if ans>=len(nums)-1:
+                    return ans+1
+                    break
+                ans+=1
+            return ans
