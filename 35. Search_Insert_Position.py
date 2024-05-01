994291cf44884a901d0a3a3f0c1dd0e58d8ff222
Brian Tseng <brianboy9999@gmail.com>
2024-05-01 12:36:05 +0800

    update-Q35

--- 35. Search_Insert_Position.py
+++ 35. Search_Insert_Position.py
@@ -8,10 +8,7 @@ class Solution(object):
         if target in nums:
             return nums.index(target)
         else:
-            ans=0
-            while nums[ans]<target:
-                if ans>=len(nums)-1:
-                    return ans+1
-                    break
-                ans+=1
-            return ans
+            for i in range(len(nums)):
+                if nums[i]>target:
+                    return i
+        return len(nums)
