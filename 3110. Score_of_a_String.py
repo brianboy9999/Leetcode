924bdf766bb5b0fc3153b8c90005febbb71b3169
Brian Tseng <brianboy9999@gmail.com>
2024-05-05 21:40:13 +0800

    update-Q3110

--- 3110. Score_of_a_String.py
+++ 3110. Score_of_a_String.py
@@ -1,6 +1,10 @@
-def score(s):
-    sum=0
-    for i in range(0,len(s)-1):
-        sum+=abs(ord(s[i])-ord(s[i+1]))
-    return sum
-print(score("zaz"))
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum=0
+        for i in range(0,len(s)-1):
+            sum+=abs(ord(s[i])-ord(s[i+1]))
+        return sum
